57fe040aa66785e99b4f3fbb6e1d9a6ae277f91b
TalithaSevrillaD <talitha_sevrilla_27rpl@student.smktelkom-mlg.sch.id>
2020-08-16 17:41:57 +0800

    1st commit

---
+++ app.js
@@ -0,0 +1,19 @@
+<html lang="en">
+    new Vue({});
+    <head>
+        <script src="https://cdn.jsdelivr.net/npm/vue">
+            
+        </script>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <div id="app">
+<h1></h1>
+        </div>
+        <script src="app.js">
+
+        </script>
+
+
+    </body>
+</html>
